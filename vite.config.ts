025b2fa879b2d4eb209effdbecc616e1d996c135
marcuoveio <marcus_vsm@live.com>
2026-05-09 23:48:07 -0300

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
-  base: "./",
-
+  base: '/pokedex/',
   server: {
     host: "::",
     port: 8080,
